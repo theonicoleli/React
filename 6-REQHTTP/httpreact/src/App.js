aaf9006b177dadb6b950e72fda773a2aa547162f
Théo Nicoleli <118020861+theonicoleli@users.noreply.github.com>
2023-11-05 18:44:03 -0300

    React
    Estudos de react correções.

--- 6-REQHTTP/httpreact/src/App.js
+++ 6-REQHTTP/httpreact/src/App.js
@@ -1,10 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
+
+const apiUrl = "http://localhost:3000/products";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(apiUrl);
+        if (!res.ok) {
+          throw new Error(`HTTP Error! Status: ${res.status}`);
+        }
+        const data = await res.json();
+        setProducts(data);
+      } catch (error) {
+        console.error("Erro ao buscar dados:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(products);
+
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - R$ {product.price}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
